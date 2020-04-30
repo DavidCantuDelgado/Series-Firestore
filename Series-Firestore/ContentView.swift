15ad538326ca11f887fa59be58d8823540f2b009
David Cantu <david.cantu.delgado@tec.mx>
2020-04-30 12:11:40 -0500

    Initial Commit

---
+++ Series-Firestore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Series-Firestore
+//
+//  Created by David A Cantú Delgado on 30/04/20.
+//  Copyright © 2020 Tec de Monterrey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
